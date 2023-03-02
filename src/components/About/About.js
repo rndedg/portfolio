c79257382782ce94347d6a902aa7ae2cad1f5e52
Gord L <rndedg@gmail.com>
2023-03-02 10:42:53 -0800

    Add icons for links, and worked on styling.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,7 +14,7 @@ function About() {
           <p>Gord grew up in a small town called Powell River on the West Coast of B.C. After the local paper mill he was working at was shut down, he took the opportunity to attend a Web Development Bootcamp through Lighthouse Labs. 
           </p>  
           <p>
-          He has always enjoyed working on and with computers and wanted to learn moreabout how things worked in the background. He hopes this new careerdirection will provide solid, long-term stability for him and his family.  
+          He has always enjoyed working on and with computers and wanted to learn more about how things worked in the background. He hopes this new career direction will provide solid, long-term stability for him and his family.  
           </p>   
         </div> 
 
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,10 @@
+import { SvgIcon } from "@mui/material";
 import { Link } from "react-router-dom";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
-
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import "./Contact.scss"
 
 function Contact() {
@@ -13,11 +16,11 @@ function Contact() {
         <Sidebar className="contact-sidebar"/>
         <div className="contact-info">
           <h1 id="page-title">Contact</h1>
-          <p>Here I will show all my contact information</p> 
+          <p>If you would like to get in touch with Gord, here are a few ways to do so:</p> 
           <ul>
-            <li><a href="mailto:g.j.letkeman@gmail.com">Send Gord an email</a></li>
-            <li><Link to="https://www.linkedin.com/in/gord-letkeman/">Visit Gord on LinkedIn</Link></li>
-            <li><Link to="https://github.com/rndedg">Check out Gord's GitHub</Link></li>
+            <li><SvgIcon component={EmailIcon}></SvgIcon><a href="mailto:g.j.letkeman@gmail.com">Send him an email</a></li>
+            <li><SvgIcon component={LinkedInIcon}></SvgIcon><Link to="https://www.linkedin.com/in/gord-letkeman/">Visit him on LinkedIn</Link></li>
+            <li><SvgIcon component={GitHubIcon}></SvgIcon><Link to="https://github.com/rndedg">Check out his GitHub</Link></li>
           </ul>       
         </div>
       </div>
